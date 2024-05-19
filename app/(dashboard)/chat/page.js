05a492a82b02076a27476ed1df7835ea6e901427
Shrey Kumar <shreykumar191@gmail.com>
2024-05-19 11:25:23 +0530

    react query

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,10 +1,17 @@
-import Chat from '@/components/Chat'
-import React from 'react'
+import Chat from "@/components/Chat";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 const ChatPage = () => {
+  const queryClient = new QueryClient();
   return (
-    <div><Chat /></div>
-  )
-}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat />
+    </HydrationBoundary>
+  );
+};
 
-export default ChatPage
+export default ChatPage;
