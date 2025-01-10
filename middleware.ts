68931e44a32a5f29f98ef9b6596c70043b8c2043
Shrey Kumar <shreykumar191@gmail.com>
2025-01-10 13:12:20 +0530

    Refactor middleware for route protection, update Prisma dependencies to version 6.2.1, add new Profile component, and initialize database migration for Tour model with direct URL support.

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isProtectedRoute = createRouteMatcher([
   '/chat(.*)',
   '/profile(.*)',
-  '/chat(.*)',
   '/tours(.*)',
+  '/landmark(.*)',
 ]);
  
 export default clerkMiddleware((auth, req) => {
